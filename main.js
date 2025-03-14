67128136327feb992464cf1afdc3b4ba81948da6
Northeastern <northeastern@Northeasterns-MacBook-Pro.local>
2025-03-14 16:06:26 -0700

    frontend init

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello from Electron 👋')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Wakey Tasky',
+        width: 400,
+        height: 600,
+        // webPreferences: {
+        //     nodeIntegration: true
+        // }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, './app/index.html'));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+});
